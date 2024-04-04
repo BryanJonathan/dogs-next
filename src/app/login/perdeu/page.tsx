ce4011c842fd6e0c05da8832cda2665199f8dee0
Jonathan Bryan <bryan.andrade@upestate.com.br>
2024-04-04 19:46:52 -0300

    feat: criacao de usuario

--- src/app/login/perdeu/page.tsx
+++ src/app/login/perdeu/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Perdeu a senha | Dogs",
+  description: "Recupere sua conta no site Dogs",
+}
 export default function PerdeuPage() {
   return (
     <main>
--- src/app/login/resetar/page.tsx
+++ src/app/login/resetar/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Resetar a sua senha | Dogs",
+  description: "Resete a sua senha no site Dogs",
+}
+
 export default function ResetarPage() {
   return (
     <main>
