d223a5abbf32a9551d77855564e475062c4f6c2f
Jonathan Bryan <bryan.andrade@upestate.com.br>
2024-04-29 22:48:12 -0300

    pagina de recuperacao de senha

--- src/app/login/perdeu/page.tsx
+++ src/app/login/perdeu/page.tsx
@@ -1,3 +1,4 @@
+import LoginPerdeuForm from "@/components/login/login-perdeu-form";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,8 +7,9 @@ export const metadata: Metadata = {
 }
 export default function PerdeuPage() {
   return (
-    <main>
-      <h1>Perdeu</h1>
-    </main>
+    <div className="animeLeft">
+      <h1 className="title">Perdeu a senha?</h1>
+      <LoginPerdeuForm />
+    </div>
   );
 }
